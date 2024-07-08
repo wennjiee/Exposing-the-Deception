6eed53a51fbd0cade81ab7413a9381d751da4b0a
wennjie <1229368677@qq.com>
2024-07-08 14:30:59 +0800

    update args for training and testing

--- parameters.py
+++ parameters.py
@@ -4,18 +4,18 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     # general arguments
-    parser.add_argument("--name", default="train_model", type=str, help="Specify name of the model")
-    parser.add_argument("--gpu_num", default="0,1,2,3", type=str,
-                        help="gpu number")
+    parser.add_argument("--name", default="waitan", type=str, help="Specify name of the model")
+    parser.add_argument("--gpu_num", default="0", type=str, help="gpu number")
+                        
 
     # arguments for train
-    parser.add_argument('--model', default="resnet", type=str, help="choose backbone model")
+    parser.add_argument('--model', default="resnet34", type=str, help="choose backbone model")
     parser.add_argument("--epoch", default=10, type=int, help="epoch of training")
     parser.add_argument("--weight_decay", default=1e-6, type=float, help="weight decay of training")
     parser.add_argument("--lr", default=1e-3, type=float, help="learning rate of training")
-    parser.add_argument("--bs", default=256, type=int, help="batch size of training")
-    parser.add_argument("--test_bs", default=1000, type=int, help="batch size of training")
-    parser.add_argument("--num_workers", default=12, type=int, help="num workers")
+    parser.add_argument("--bs", default=128, type=int, help="batch size of training")
+    parser.add_argument("--test_bs", default=128, type=int, help="batch size of training")
+    parser.add_argument("--num_workers", default=4, type=int, help="num workers")
 
     # arguments for loss
     parser.add_argument("--lil_loss", default=True, type=bool, help="if local information loss")
@@ -25,10 +25,10 @@ def parse_args():
     parser.add_argument('--scales', default=[1,2,10], type=list, help="multiple losses weights")
     parser.add_argument('--balance_loss_method', default='auto', type=str, help="balance multiple losses method")
 
-    #model parameters
+    # model parameters
     parser.add_argument("--num_LIBs", default=4, type=int, help="the number of Local Information Block")
     parser.add_argument("--resume_model",
-                        default="",
+                        default="output/train_waitan/model_epoch_8.pth", # output/train_celeb_df_v2/model_best. output/train_race/model_best.pth
                         type=str,
                         help="Path of resume model")
 
@@ -36,16 +36,14 @@ def parse_args():
     parser.add_argument("--test", default=False, type=bool,help="Test or not")
 
     # save models
-    parser.add_argument("--save_model", default=True, type=bool,help="whether save models or not")
-    parser.add_argument("--save_path", default="output", type=str,
-                        help="Path of test file, work when test is true")
-
+    parser.add_argument("--save_model", default=True, type=bool, help="whether save models or not")
+    parser.add_argument("--save_path", default="output", type=str, help="Path of test file, work when test is true")
+    
     # dataset
-    parser.add_argument("--size", default=224, type=int,
-                        help="Specify the size of the input image, applied to width and height")
-    parser.add_argument('--dataset', default="FF++", type=str, help="dataset txt path")#
-    # 'Face2Face','Deepfakes','FaceSwap','NeuralTextures', Celeb-DF-v2, DFDC-Preview,DFDC,FF++_c23,DeeperForensics-1.0
-    parser.add_argument("--mixup", default=True, type=bool,help="mix up or not")
+    parser.add_argument("--size", default=224, type=int, help="Specify the size of the input image, applied to width and height")
+    parser.add_argument('--dataset', default="waitan", type=str, help="dataset txt path")
+    # 'Face2Face','Deepfakes','FaceSwap','NeuralTextures', Celeb-DF-v2, DFDC-Preview, DFDC, FF++_c23, DeeperForensics-1.0, cifar-10-batches-py
+    parser.add_argument("--mixup", default=True, type=bool, help="mix up or not")
     parser.add_argument("--alpha", default=0.5, type=float, help="mix up alpha")
 
     return parser.parse_args()
