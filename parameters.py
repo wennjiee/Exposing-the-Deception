57d42813f3a95ea5c5b2e3eee6de8ebd95a15eb5
wennjie <1229368677@qq.com>
2024-07-11 20:27:15 +0800

    update args for training

--- parameters.py
+++ parameters.py
@@ -4,7 +4,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     # general arguments
-    parser.add_argument("--name", default="deepfake", type=str, help="Specify name of the model")
+    parser.add_argument("--name", default="deepfake_kaggle", type=str, help="Specify name of the model")
     parser.add_argument("--gpu_num", default="0", type=str, help="gpu number")
                         
 
@@ -13,8 +13,8 @@ def parse_args():
     parser.add_argument("--epoch", default=10, type=int, help="epoch of training")
     parser.add_argument("--weight_decay", default=1e-6, type=float, help="weight decay of training")
     parser.add_argument("--lr", default=1e-3, type=float, help="learning rate of training")
-    parser.add_argument("--bs", default=128, type=int, help="batch size of training")
-    parser.add_argument("--test_bs", default=128, type=int, help="batch size of training")
+    parser.add_argument("--bs", default=72, type=int, help="batch size of training")
+    parser.add_argument("--test_bs", default=72, type=int, help="batch size of training")
     parser.add_argument("--num_workers", default=6, type=int, help="num workers")
 
     # arguments for loss
@@ -28,7 +28,7 @@ def parse_args():
     # model parameters
     parser.add_argument("--num_LIBs", default=4, type=int, help="the number of Local Information Block")
     parser.add_argument("--resume_model",
-                        default="", # output/train_celeb_df_v2/model_best. output/train_race/model_best.pth
+                        default="output/train_deepfake_kaggle/model_epoch_1.pth", # output/train_celeb_df_v2/model_best. output/train_race/model_best.pth
                         type=str,
                         help="Path of resume model")
 
